13b1e19066bd4325737d63aac54054838c8fb899
aadev <aadevmobile@gmail.com>
2025-01-14 19:37:36 -0500

    Initial Commit

---
+++ bbquotes/bbquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  bbquotesApp.swift
+//  bbquotes
+//
+//  Created by Anie on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct bbquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
